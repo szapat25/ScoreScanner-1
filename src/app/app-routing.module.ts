1b27d03fc50ebda953273035bd38893238256b6c
Alejandra <acarden6@eafit.edu.co>
2019-04-08 22:51:09 -0500

    Pequeño avance con el front

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'inicio', loadChildren: './pages/inicio/inicio.module#InicioPageModule' },
+  { path: 'scanner', loadChildren: './pages/scanner/scanner.module#ScannerPageModule' },
+  { path: 'biblioteca', loadChildren: './pages/biblioteca/biblioteca.module#BibliotecaPageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
