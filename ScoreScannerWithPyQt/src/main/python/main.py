98168eeab2846416e2e5481b91d964c71faf328d
Alejandra <acarden6@eafit.edu.co@github.com>
2019-05-13 02:18:37 -0500

    Se agrega la función de adjuntar (por mejorar)

--- ScoreScannerWithPyQt/src/main/python/main.py
+++ ScoreScannerWithPyQt/src/main/python/main.py
@@ -1,15 +1,25 @@
 from fbs_runtime.application_context import ApplicationContext
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QWidget, QPushButton, QFileDialog
 from PyQt5 import QtGui, QtCore, QtWidgets
 from Interfaz import *
 
 import sys
+
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent=parent)
         self.setWindowTitle('ScoreScanner')
         self.setupUi(self)
+        boton_Adjuntar = QPushButton('Adjuntar',self)
+        boton_Adjuntar.clicked.connect(self.AdjuntarImagen)
+
+    def AdjuntarImagen(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
+        if fileName:
+            print(fileName)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
