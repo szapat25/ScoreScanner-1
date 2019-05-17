d0a3f44d8a6fd2518f5da952c084500365de9b66
Alejandra <acarden6@eafit.edu.co@github.com>
2019-05-17 07:11:26 -0500

    Procesar

--- ScoreScannerWithPyQt/src/main/python/main.py
+++ ScoreScannerWithPyQt/src/main/python/main.py
@@ -1,12 +1,14 @@
 from fbs_runtime.application_context import ApplicationContext
-from PyQt5.QtWidgets import QMainWindow, QWidget, QPushButton, QFileDialog
+from PyQt5.QtWidgets import QMainWindow, QWidget, QPushButton, QFileDialog, QProgressBar, QDialog, QMessageBox
 from PyQt5 import QtGui, QtCore, QtWidgets, uic
 import Interfaz
 from Interfaz import *
 from Interfaz import Ui_MainWindow
 
 import sys
+import time
 
+TIME_LIMIT = 100
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
@@ -19,9 +21,15 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         #boton_Adjuntar = QPushButton('Adjuntar',self)
         #boton_Adjuntar.clicked.connect(self.AdjuntarImagen)
         self.frame_RegistroP.hide()
+        self.frame_GestorP.hide()
+        self.progress = QProgressBar(self)
+        self.progress.setGeometry(650, 350, 300, 50)
+        self.progress.hide()
+        self.progress.setMaximum(100)
         self.boton_RegistroP.clicked.connect(self.RegistroPartituras)
         #self.boton_cerrarRP.clicked.connect(self.CerrarFrame(self.frame_RegistroP))
         self.boton_Adjuntar.clicked.connect(self.AdjuntarImagen)
+        self.boton_Procesar.clicked.connect(self.ProcesarImagen)
 
     def RegistroPartituras(self):
         self.frame_RegistroP.show()
@@ -32,12 +40,24 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         fileName, _ = QFileDialog.getOpenFileName(self, "Choose Contact Icon", "", "Image Files (*.jpg *.pdf)",'/home')
         if fileName:
             print(fileName)
-            self.label_Partitura.setPixmap(QtGui.QPixmap(fileName).scaled(731, 491))
-
-        
+            self.label_Partitura.setPixmap(QtGui.QPixmap(fileName).scaled(731, 491))   
     
-    # def CerrarFrame(self, frameACerrar):
-    #     frameACerrar.hide()
+    def ProcesarImagen(self):        
+        self.progress.show()
+        count = 0
+        while count < TIME_LIMIT:
+            count += 1
+            time.sleep(0.2)
+            self.progress.setValue(count)
+        self.progress.hide()
+
+        msg = QMessageBox()
+        msg.setGeometry(650, 350, 300, 50)
+        msg.setIcon(QMessageBox.Information)
+        msg.setText("La imagen ha sido procesada")
+        msg.setWindowTitle("Proceso Terminado")
+        msg.exec_()
+        
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     w = MainWindow()
